e65d189a92ba6a4353545f9ece6e28a8e9525b56
abikonya <36229338+abikonya@users.noreply.github.com>
2019-03-17 14:08:35 +0700

    add config file

--- bot_app/views.py
+++ bot_app/views.py
@@ -9,18 +9,7 @@ bot = telebot.TeleBot(config.token)
 def command_start(message):
     bot.send_message(message.chat.id, "Bots webhook is working")
 
+
 def index(request):
-    if 'content-length' in request.headers and 'content-type' in request.headers \
-            and request.headers['content-type'] == 'application/json':
-        length = int(request.headers['content-length'])
-        json_string = request.body.read(length)
-        json_string = json_string.decode("utf-8")
-        update = telebot.types.Update.de_json(json_string)
-        if update.message:
-            bot.process_new_messages([update.message])
-        if update.inline_query:
-            bot.process_new_inline_query([update.inline_query])
-        print(request.headers)
-        return ''
-    else:
-        raise HttpResponseServerError
+        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+        return "!", 200
