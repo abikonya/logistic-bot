1ccdc80a9f43f8268dfba68fa4eec198b15d2458
abikonya <36229338+abikonya@users.noreply.github.com>
2019-03-17 14:38:54 +0700

    add config file

--- bot_app/views.py
+++ bot_app/views.py
@@ -5,9 +5,9 @@ from . import config
 bot = telebot.TeleBot(config.token)
 
 
-@bot.message_handler(commands=["start"])
-def command_start(message):
-    bot.send_message(message.chat.id, "Bots webhook is working")
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, 'Bot is working')
 
 
 def index(request):
