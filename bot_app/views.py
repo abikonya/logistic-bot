14a35913bd380579c7405490ac6e7ad0611ba234
abikonya <36229338+abikonya@users.noreply.github.com>
2019-03-19 21:40:13 +0700

    webhook is configured

--- bot_app/views.py
+++ bot_app/views.py
@@ -32,7 +32,7 @@ def lang_select(call):
     global language
     if call.data == 'ru':
         language = 'ru'
-        bot.send_message(localization.rules[language], call.message.chat_id)
+        bot.send_message(text=localization.rules[language], chat_id=call.message.chat.id)
     else:
         language = 'en'
-        bot.send_message(localization.rules[language], call.message.chat_id)
+        bot.send_message(text=localization.rules[language], chat_id=call.message.chat.id)
