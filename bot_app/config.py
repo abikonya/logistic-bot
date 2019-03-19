f106ea8b6830e927cef2f1135a361b9e4df23ded
abikonya <36229338+abikonya@users.noreply.github.com>
2019-03-19 15:48:43 +0700

    webhook is configured

--- bot_app/config.py
+++ bot_app/config.py
@@ -1,3 +1,3 @@
-bots_site = 'https://test-logisticbot.herokuapp.com/827637721:AAHZAkFOMR3bSdLWhMvRktHvzR738zj2qdM/'
+# bots_site = 'https://test-logisticbot.herokuapp.com/827637721:AAHZAkFOMR3bSdLWhMvRktHvzR738zj2qdM/'
 
 token = '827637721:AAHZAkFOMR3bSdLWhMvRktHvzR738zj2qdM'
--- bot_app/views.py
+++ bot_app/views.py
@@ -1,7 +1,9 @@
 import telebot
-from . import config
+from telebot import types
+from . import config, api_functions
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 
 
 bot = telebot.TeleBot(config.token)
@@ -19,3 +21,12 @@ class UpdateBot(APIView):
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     bot.reply_to(message, 'Bot is working')
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    keyboard = types.InlineKeyboardMarkup(row_width=2)
+    button_en = types.InlineKeyboardButton(text='English', callback_data='en')
+    button_ru = types.InlineKeyboardButton(text='Русский', callback_data='ru')
+    keyboard.add(button_en, button_ru)
+    bot.send_message(message.chat.id, 'Choose your language\n Выберите Ваш язык:', reply_markup=keyboard)
