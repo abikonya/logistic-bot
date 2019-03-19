e8328c3a768ea86425ae124c5396d1a49693a8c6
abikonya <36229338+abikonya@users.noreply.github.com>
2019-03-19 12:38:22 +0700

    webhook is configured

--- bot_app/views.py
+++ bot_app/views.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot(config.token)
 
 
 class UpdateBot(APIView):
-    def post(self,request):
+    def post(self, request):
         json_string = request.body.decode("UTF-8")
         update = telebot.types.Update.de_json(json_string)
         bot.process_new_updates([update])
@@ -19,4 +19,3 @@ class UpdateBot(APIView):
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     bot.reply_to(message, 'Bot is working')
-
