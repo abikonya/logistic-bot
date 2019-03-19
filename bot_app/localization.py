5c810b50522d5e5a3c5d9cf1e5420559eb2ec7c7
abikonya <36229338+abikonya@users.noreply.github.com>
2019-03-19 22:59:11 +0700

    webhook is configured

--- bot_app/localization.py
+++ bot_app/localization.py
@@ -8,4 +8,55 @@ Translations variables file.
 rules = {'en': 'Read rules before using', 'ru': 'Прочтите правила использования бота - (ссылка на правила)'}
 rules_button = {'en': 'ok', 'ru': 'С правилами ознакомлен'}
 
-zip_searching = {'en': '', 'ru': ''}
+# Левая ветка. Left branch
+
+zip_searching = {'en': 'Enter command /zip to search Zip-code', 'ru': 'Введите команду /zip для поиска необходимого Zip-кода'}
+
+enter_zip = {'en': '', 'ru': 'Введите цифровой номер (Zip-код)'}
+
+zip_list_choose = {'en': '', 'ru': 'Выберите один из Zip-ов'}
+zip_list = {'en': '', 'ru': 'Вы выбрали - {Zip-код}'}
+zip_list_button = {'en': '', 'ru': 'Посмотреть список товаров'}
+
+choosen_zip_approve = {'en': 'Approve', 'ru': 'Принять'}
+choosen_zip_reset = {'en': 'Reset', 'ru': 'Сбросить'}
+
+about_cargo = {'en': '', 'ru': 'Введите информацию о товаре'}
+
+pickup_location = {'en': 'Pickup Location', 'ru': 'Точка погрузки'}
+
+store_name = {'en': 'Store name', 'ru': 'Название магазина'}
+
+store_phone = {'en': 'Store phone(optional)', 'ru': 'Телефонный номер магазина (если есть)'}
+
+order_number = {'en': 'Order number', 'ru': 'Номер заказа'}
+
+pickup_person = {'en': 'Pickup person', 'ru': ''}
+
+additional_info = {'en': 'Any additional information required for pick up(optional)', 'ru': 'Дополнительная информация'}
+
+product_name = {'en': 'Product name', 'ru': ''}
+
+price = {'en': 'Price', 'ru': 'Цена'}
+
+succes = {'en': '', 'ru': 'Заказ успешно создан! Ваш ID-заказа:'}
+succes_button = {'en': 'Main menu', 'ru': 'Главное меню'}
+
+
+# Правая ветка. Right branch
+
+status_check = {'en': 'Enter command /status to check status of order', 'ru': 'Введите команду /status для просмотра статуса заказа'}
+
+enter_id = {'en': '', 'ru': 'Введите ID-заказа'}
+
+status = {'en': '', 'ru': 'Статус Вашего заказа'}
+status_process = {'en': '', 'ru': 'В процессе'}
+status_confirm = {'en': '', 'ru': 'Исполнено'}
+status_cancel = {'en': '', 'ru': 'Отклонено'}
+
+enter_requisites = {'en': '', 'ru': 'Введите свои платежные реквизиты'}
+enter_requisites_button = {'en': '', 'ru': 'Отправить'}
+
+payment_info = {'en': '', 'ru': 'Информация о расчетах'}
+
+paid_status = {'en': 'Paid', 'ru': 'Оплачено'}
