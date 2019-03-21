8a661b8374f9609862345699f256a7c78ad50aea
abikonya <36229338+abikonya@users.noreply.github.com>
2019-03-21 17:41:53 +0700

    get_distance

--- bot_app/views.py
+++ bot_app/views.py
@@ -1,6 +1,7 @@
 import telebot
 from telebot import types
 import re
+import requests
 from . import config, api_functions, localization
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -66,5 +67,5 @@ def main(message):
 @bot.message_handler(func=lambda message: re.search(r'^[0-9]{5}$', message.text))
 def zip_list(message):
     api_functions.attrib['zipcode'] = message.text
-    print(api_functions.get_distance)
+    print(requests.get(api_functions.get_distance))
     bot.send_message(message.chat.id, 'Ok')
