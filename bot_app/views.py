bb6f416ed19b278d3f4991c754555bfe60eb830a
abikonya <36229338+abikonya@users.noreply.github.com>
2019-03-21 14:40:34 +0700

    Status_check + zip_searching

--- bot_app/views.py
+++ bot_app/views.py
@@ -47,6 +47,7 @@ def lang_select(call):
 @bot.message_handler(func=lambda message: message.text == localization.rules_button[language])
 def main(message):
     global language
-    bot.send_message(message.chat.id, localization.zip_searching + localization.status_check, reply_markup=types.ReplyKeyboardRemove)
+    bot.send_message(message.chat.id, localization.zip_searching[language] + localization.status_check[language],
+                     reply_markup=types.ReplyKeyboardRemove)
 
 
