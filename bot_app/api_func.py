f3e20d4a5c932f922b3853e19d72a92980a57a07
abikonya <36229338+abikonya@users.noreply.github.com>
2019-03-26 10:08:44 +0700

    get_distance TEST

--- bot_app/api_func.py
+++ bot_app/api_func.py
@@ -132,7 +132,7 @@ class Api:
         try:
             request = requests.get('https://strongbox.cc/?a=fnc.api.package.getstatus&tuser={user_id}'.format(
                 user_id=self.user_id))
-            return json.loads(request.text)
+            return request.text
         except Exception as err:
             print(err)
 
