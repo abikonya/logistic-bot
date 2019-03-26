7a8eda4bd1dc96b60c00eb29e7e2fce041c86f7b
abikonya <36229338+abikonya@users.noreply.github.com>
2019-03-26 11:26:58 +0700

    get_distance TEST

--- bot_app/views.py
+++ bot_app/views.py
@@ -78,7 +78,7 @@ def zip_list(message):
     global language, api_instance
     api_instance.set_zipcode(message.text)
     get_distance = api_instance.get_distance()
-    if type(get_distance['address']) == list() and get_distance['address']:
+    if type(get_distance) == dict() and get_distance['address']:
         couriers_list = sorted(get_distance['address'], key=sort_by_dist)
         keyboard = types.InlineKeyboardMarkup()
         for each in couriers_list:
