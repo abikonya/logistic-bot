88cdec1159af1bb1593b557880aaad9154010440
abikonya <36229338+abikonya@users.noreply.github.com>
2019-03-29 13:12:08 +0700

    testing

--- bot_app/localization.py
+++ bot_app/localization.py
@@ -6,22 +6,22 @@ Translations variables file.
 
 
 rules = {'en': 'Read rules before using', 'ru': 'Прочтите правила использования бота - (ссылка на правила)'}
-rules_button = {'en': 'ok', 'ru': 'С правилами ознакомлен'}
+rules_button = {'en': 'Agree', 'ru': 'С правилами ознакомлен'}
 
 # Левая ветка. Left branch
 
 zip_searching = {'en': 'Enter command /zip to search Zip-code', 'ru': 'Введите команду /zip для поиска необходимого Zip-кода'}
 
-enter_zipcode = {'en': 'Ok', 'ru': 'Введите цифровой номер (Zip-код)'}
+enter_zipcode = {'en': 'Enter zip', 'ru': 'Введите цифровой номер (Zip-код)'}
 
 zip_list_choose = {'en': 'Make your choice please:', 'ru': 'Выберите один из Zip-ов'}
-zip_list = {'en': '', 'ru': 'Вы выбрали - {Zip-код}'}
-zip_list_button = {'en': '', 'ru': 'Посмотреть список товаров'}
+zip_list = {'en': 'You selected - {Zip}', 'ru': 'Вы выбрали - {Zip}'}
+zip_list_button = {'en': 'Show list of goods', 'ru': 'Посмотреть список товаров'}
 
 choosen_zip_approve = {'en': 'Confirm', 'ru': 'Принять'}
 choosen_zip_reset = {'en': 'Reset', 'ru': 'Сбросить'}
 
-about_cargo = {'en': '', 'ru': 'Введите информацию о товаре'}
+about_cargo = {'en': 'Enter product info', 'ru': 'Введите информацию о товаре'}
 
 pickup_location = {'en': 'Pickup Location', 'ru': 'Точка погрузки'}
 
@@ -31,18 +31,18 @@ store_phone = {'en': 'Store phone(optional)', 'ru': 'Телефонный ном
 
 order_number = {'en': 'Order number', 'ru': 'Номер заказа'}
 
-pickup_person = {'en': 'Pickup person', 'ru': ''}
+pickup_person = {'en': 'Pickup person', 'ru': 'Получатель'}
 
 additional_info = {'en': 'Any additional information required for pick up(optional)', 'ru': 'Дополнительная информация'}
 
-product_name = {'en': 'Product name', 'ru': ''}
+product_name = {'en': 'Product name', 'ru': 'Название товара'}
 
 price = {'en': 'Price', 'ru': 'Цена'}
 send_button = {'en': 'Send', 'ru': 'Отправить'}
 
 all_is_done = {'en': 'The data entered is correct', 'ru': 'Введенные данные верны'}
 
-success = {'en': '', 'ru': 'Заказ успешно создан! Ваш ID-заказа:'}
+success = {'en': 'Order created success! Your order-ID:', 'ru': 'Заказ успешно создан! Ваш ID-заказа:'}
 success_button = {'en': 'Main menu', 'ru': 'Главное меню'}
 
 
@@ -50,16 +50,16 @@ success_button = {'en': 'Main menu', 'ru': 'Главное меню'}
 
 status_checking = {'en': 'Enter command /status to check status of order', 'ru': 'Введите команду /status для просмотра статуса заказа'}
 
-enter_id = {'en': '', 'ru': 'Введите ID-заказа'}
+enter_id = {'en': 'Enter order-ID:', 'ru': 'Введите ID-заказа'}
 
-status = {'en': '', 'ru': 'Статус Вашего заказа'}
-status_process = {'en': '', 'ru': 'В процессе'}
-status_confirm = {'en': '', 'ru': 'Исполнено'}
-status_cancel = {'en': '', 'ru': 'Отклонено'}
+status = {'en': "Your order's status", 'ru': 'Статус Вашего заказа'}
+status_process = {'en': 'Process', 'ru': 'В процессе'}
+status_confirm = {'en': 'Confirm', 'ru': 'Исполнено'}
+status_cancel = {'en': 'Rejected', 'ru': 'Отклонено'}
 
-enter_requisites = {'en': '', 'ru': 'Введите свои платежные реквизиты'}
-enter_requisites_button = {'en': '', 'ru': 'Отправить'}
+enter_requisites = {'en': 'Enter your payment requisites', 'ru': 'Введите свои платежные реквизиты'}
+enter_requisites_button = {'en': 'Send', 'ru': 'Отправить'}
 
-payment_info = {'en': '', 'ru': 'Информация о расчетах'}
+payment_info = {'en': 'Information about payments', 'ru': 'Информация о расчетах'}
 
 paid_status = {'en': 'Paid', 'ru': 'Оплачено'}
