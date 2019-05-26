bd8fdfe793224a3cd9d6b818a4ea083746342708
abikonya <36229338+abikonya@users.noreply.github.com>
2019-05-26 14:45:44 +0700

    add product model and dbworker

--- bot_app/dbworker.py
+++ bot_app/dbworker.py
@@ -3,14 +3,15 @@ from . import api_func
 
 
 def add_product(telegram_id):
-    Products.user_id = api_func.return_param(telegram_id, 'user_id')
-    Products.zipcode = api_func.return_param(telegram_id, 'zipcode')
-    Products.store_name = api_func.return_param(telegram_id, 'store_name')
-    Products.store_phone = api_func.return_param(telegram_id, 'store_phone')
-    Products.order_number = api_func.return_param(telegram_id, 'order_number')
-    Products.pickup_person = api_func.return_param(telegram_id, 'pickup_person')
-    Products.pickup_location = api_func.return_param(telegram_id, 'pickup_location')
-    Products.more_info = api_func.return_param(telegram_id, 'more_info')
-    Products.product_category = api_func.return_param(telegram_id, 'product_category')
-    Products.product_item = api_func.return_param(telegram_id, 'product_item')
-    Products.price = api_func.return_param(telegram_id, 'price')
+    query = Products(user_id = api_func.return_param(telegram_id, 'user_id'),
+                     zipcode = api_func.return_param(telegram_id, 'zipcode'),
+                     store_name = api_func.return_param(telegram_id, 'store_name'),
+                     store_phone = api_func.return_param(telegram_id, 'store_phone'),
+                     order_number = api_func.return_param(telegram_id, 'order_number'),
+                     pickup_person = api_func.return_param(telegram_id, 'pickup_person'),
+                     pickup_location = api_func.return_param(telegram_id, 'pickup_location'),
+                     more_info = api_func.return_param(telegram_id, 'more_info'),
+                     product_category = api_func.return_param(telegram_id, 'product_category'),
+                     product_item = api_func.return_param(telegram_id, 'product_item'),
+                     price = api_func.return_param(telegram_id, 'price'))
+    query.save()
