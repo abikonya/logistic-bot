0451304ffcf35433499c8206340f15c811e6b336
abikonya <36229338+abikonya@users.noreply.github.com>
2019-05-26 16:00:44 +0700

    add product model and dbworker

--- bot_app/models.py
+++ bot_app/models.py
@@ -16,16 +16,16 @@ class AuthorizedCustomers(models.Model):
 
 class Products(models.Model):
     user_id = models.CharField(max_length=50, null=False, verbose_name='User ID')
-    zipcode = models.IntegerField(null=False, verbose_name='Zipcode')
+    zipcode = models.CharField(max_length=50, null=False, verbose_name='Zipcode')
     store_name = models.CharField(max_length=50, null=True, verbose_name='Название магазина')
-    store_phone = models.IntegerField(null=True, verbose_name='Телефон магазина')
-    order_number = models.IntegerField(null=False, verbose_name='Номер заказа')
+    store_phone = models.CharField(max_length=50, null=True, verbose_name='Телефон магазина')
+    order_number = models.CharField(max_length=50, null=False, verbose_name='Номер заказа')
     pickup_person = models.CharField(max_length=50, null=False, verbose_name='Получатель')
     pickup_location = models.CharField(max_length=50, null=False, verbose_name='Точка погрузки')
     more_info = models.TextField(null=True, verbose_name='Дополнительное инфо')
     product_category = models.CharField(max_length=50, null=False, verbose_name='Категория товара')
     product_item = models.CharField(max_length=50, null=False, verbose_name='Товар')
-    price = models.DecimalField(decimal_places=3, max_digits=50, null=False, verbose_name='Цена')
+    price = models.CharField(max_length=50, null=False, verbose_name='Цена')
     created = models.DateTimeField(max_length=50, auto_now_add=True, db_index=True, verbose_name='Дата добавления')
 
     class Meta:
