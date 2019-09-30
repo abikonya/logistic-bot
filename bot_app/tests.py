3e9e6537bf7efd16bd3e87413f7b8a0cefcd4117
Anton <36229338+abikonya@users.noreply.github.com>
2019-09-29 23:26:10 -0700

    Update tests.py
    deleted unused tests

--- bot_app/tests.py
+++ bot_app/tests.py
@@ -1,16 +1,3 @@
 # from django.test import TestCase
 #
 # # Create your tests here.
-
-
-import requests
-import json
-
-
-currency_url = 'https://blockchain.info/ticker'
-
-currency = requests.get(currency_url)
-
-currency_to_usd = json.loads(currency.text)['USD']
-
-print(currency_to_usd)
